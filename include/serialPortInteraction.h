ea8cf8b82d680840422194cb39009f541ae506d0
Arthur Koba <caprimores@gmail.com>
2023-01-28 06:04:38 +0300

    add sendAmplitudesArea

--- include/serialPortInteraction.h
+++ include/serialPortInteraction.h
@@ -25,6 +25,33 @@ void sendJsonArray(float *arr, const uint16_t arrSize, String dataType = "null")
   Serial.println("]}");
 }
 
+void sendAmplitudesArea(float *arr, const uint16_t arrSize, String dataType = "null", uint8_t area = 0) {
+    sendDataType(dataType);
+    uint16_t startIndex, stopIndex;
+    switch (area) {
+        case 1:
+            startIndex = arrSize/4;
+            stopIndex = arrSize/2;
+            break;
+        case 2:
+            startIndex = arrSize/2;
+            stopIndex = arrSize*3/4;
+            break;
+        case 3:
+            startIndex = arrSize*3/4;
+            stopIndex = arrSize;
+            break;
+        case 0:
+        default:
+            startIndex = 0;
+            stopIndex = arrSize/4;
+    }
+
+    Serial.print("[" + String(arr[startIndex]));
+    for (uint16_t i = startIndex + 1; i < stopIndex;) Serial.print("," + String(arr[i++]));
+    Serial.println("]}");
+}
+
 
 // void sendSamples(SamplingConfigs &sampling) {
 //   sendJsonArray(sampling.samples, sampling.samplesLength, "samplesData");
