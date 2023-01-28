543584a7cbe0619bfb7ddc4025264aa379c16f04
Arthur Koba <caprimores@gmail.com>
2023-01-28 06:35:56 +0300

    Refactor | move to include dir, update styles

--- include/encoder.h
+++ include/encoder.h
@@ -10,27 +10,32 @@
   #define ENCODER_DELTA_TIME_MS 50
 #endif
 
-struct ENCODER_DATA {
-  bool isUpdated = false;
-  uint8_t rotateState = 0;
-  uint32_t lastUpdateAngle=0;
-#ifdef ENCODER_BTN_PIN
-  bool isPressed = false;
-  bool isClicked = false;
-  uint32_t lastUpdateBTN=0;
-#endif
 
-} encoderData;
+struct EncoderData {
+    bool isUpdated = false;
+    uint8_t rotateState = 0;
+    uint32_t lastUpdateAngle=0;
+#ifdef ENCODER_BTN_PIN
+    bool isPressed = false;
+    bool isClicked = false;
+    uint32_t lastUpdateBTN=0;
+#endif
+};
+
+
+EncoderData encoderData;
+
 
 void IRAM_ATTR isrEncoderAB() {
-  bool dt = digitalRead(ENCODER_DT_PIN);
-  if (dt == digitalRead(ENCODER_CLK_PIN)) return;
-  uint32_t time = millis();
-  if (time - encoderData.lastUpdateAngle < ENCODER_DELTA_TIME_MS) return;
-  encoderData.rotateState = dt ? 1 : 2;
-  encoderData.isUpdated = true;
-  encoderData.lastUpdateAngle = time;
+    bool dt = digitalRead(ENCODER_DT_PIN);
+    if (dt == digitalRead(ENCODER_CLK_PIN)) return;
+    uint32_t time = millis();
+    if (time - encoderData.lastUpdateAngle < ENCODER_DELTA_TIME_MS) return;
+    encoderData.rotateState = dt ? 1 : 2;
+    encoderData.isUpdated = true;
+    encoderData.lastUpdateAngle = time;
 }
+
 
 #ifdef ENCODER_BTN_PIN
 void IRAM_ATTR isrEncoderBTN() {
@@ -43,18 +48,20 @@ void IRAM_ATTR isrEncoderBTN() {
 }
 #endif
 
+
 void setupEncoder() {
-  pinMode(ENCODER_CLK_PIN, INPUT_PULLUP);
-  pinMode(ENCODER_DT_PIN, INPUT_PULLUP);
-  attachInterrupt(ENCODER_CLK_PIN, isrEncoderAB, CHANGE);
-  attachInterrupt(ENCODER_DT_PIN, isrEncoderAB, CHANGE);
+    pinMode(ENCODER_CLK_PIN, INPUT_PULLUP);
+    pinMode(ENCODER_DT_PIN, INPUT_PULLUP);
+    attachInterrupt(ENCODER_CLK_PIN, isrEncoderAB, CHANGE);
+    attachInterrupt(ENCODER_DT_PIN, isrEncoderAB, CHANGE);
 #ifdef ENCODER_BTN_PIN
-  pinMode(ENCODER_BTN_PIN, INPUT_PULLUP);
-  attachInterrupt(ENCODER_BTN_PIN, isrEncoderBTN, CHANGE);
-  encoderData.lastUpdateBTN = millis();
+    pinMode(ENCODER_BTN_PIN, INPUT_PULLUP);
+    attachInterrupt(ENCODER_BTN_PIN, isrEncoderBTN, CHANGE);
+    encoderData.lastUpdateBTN = millis();
 #endif
-  encoderData.lastUpdateAngle = millis();
+    encoderData.lastUpdateAngle = millis();
 }
+
 
 void execEncoder(BluetoothA2DPSink &a2dp_sink) {
     if (!encoderData.isUpdated) return;
