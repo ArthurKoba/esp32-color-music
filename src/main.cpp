924c825e9f9482afc4da2bbb2af0ef5bae12b770
Arthur Koba <caprimores@gmail.com>
2023-01-28 06:05:47 +0300

    add ws2812, colorModes, ir.h. Configure all

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,9 @@
 #define ENCODER_DT_PIN 33
 #define ENCODER_BTN_PIN 27
 
+#define IR_RECEIVE_PIN 13
+#define WS2812B_PIN 12
+
 #define I2S_BCK_PIN 14
 #define I2S_DATA_OUT_PIN 25
 #define I2S_WS_PIN 26
@@ -9,27 +12,110 @@
 #define BLUETOOTH_DEVICE_NAME "Koba Audio Receiver"
 
 #include "Arduino.h"
+
 #include "serialPortInteraction.h"
+#include "ws2812.h"
 #include "colorMusic.h"
+#include "colorModes.h"
+
 #include "audioReceiver.h"
 #include "encoder.h"
+#include "ir.h"
 
 
 void setup() {
     Serial.begin(500000);
     setupA2DP();
     a2dp_sink.set_volume(64);
-    setupEncoder();
+//    setupEncoder();
+//    setupFastLED();
+    setupColorModes();
     setupColorMusic();
+    setupIR();
+    Serial.print('\n');
     Serial.print(BLUETOOTH_DEVICE_NAME);
     Serial.println(" Started!");
+
+    Serial.print("samples size: ");
+    Serial.println(SAMPLES_SIZE);
+    Serial.print("ampl size: ");
+    Serial.println(AMPLITUDES_SIZE);
+
+}
+
+uint8_t fftArea = 0;
+bool sendWindow = false;
+
+void colorMusic() {
+    if (fftData.samples.fullness != SAMPLES_SIZE) return;
+//    sendJsonArray(fftData.samples.left, 256, "samples");
+    uint16_t calcTime = millis();
+    calculateAmplitudes(fftData.samples.right, fftData.amplitudes.right);
+    calculateAmplitudes(fftData.samples.left, fftData.amplitudes.left);
+    calcTime = millis() - calcTime;
+    Serial.print("Calc time (ms): " + String(calcTime));
+    Serial.println(" Area: " + String(fftArea));
+    if (sendWindow) {
+        sendJsonArray(fftData.fftWindow, SAMPLES_SIZE, "test");
+    } else {
+        sendAmplitudesArea(fftData.amplitudes.left, AMPLITUDES_SIZE, "test", fftArea);
+    }
+
+//    calcTime = millis() - calcTime;
+//    Serial.println("Calc time (ms): " + String(calcTime));
+    fftData.samples.fullness = 0;
+}
+
+void execIrCommandTest() {
+    if (!IrReceiver.decode()) return;
+    uint16_t command = IrReceiver.decodedIRData.command;
+    Serial.println(String(command));
+    switch (command) {
+        case OFF_BUTTON:    colorModes.mode = COLOR_MODE;           break;
+        case ON_BUTTON:     colorModes.mode = COLOR_MUSIC_MODE;     break;
+        case 4:             fftData.useWindow = false;              break;
+        case 5:             fftData.useWindow = true;               break;
+        case 6:             fftData.logarithmResult = false;        break;
+        case 7:             fftData.logarithmResult = true;         break;
+        case 8:             fftArea = 0;         break;
+        case 9:             fftArea = 1;         break;
+        case 10:             fftArea = 2;         break;
+        case 11:             fftArea = 3;         break;
+        case 12: dsps_wind_blackman_f32(fftData.fftWindow, SAMPLES_SIZE); break;
+        case 13: dsps_wind_blackman_harris_f32(fftData.fftWindow, SAMPLES_SIZE); break;
+        case 14: dsps_wind_blackman_nuttall_f32(fftData.fftWindow, SAMPLES_SIZE); break;
+        case 15: dsps_wind_flat_top_f32(fftData.fftWindow, SAMPLES_SIZE); break;
+        case 16: dsps_wind_hann_f32(fftData.fftWindow, SAMPLES_SIZE); break;
+        case 17: dsps_wind_nuttall_f32(fftData.fftWindow, SAMPLES_SIZE); break;
+        case 18: sendWindow = false; break;
+        case 19: sendWindow = true; break;
+
+
+//        case FLASH_BUTTON:  colorModes.mode = FLASH_MODE;       break;
+//        case STROBE_BUTTON: colorModes.mode = STROBE_MODE;      break;
+//        case FADE_BUTTON:   colorModes.mode = FADE_MODE;        break;
+//        case SMOOTH_BUTTON: colorModes.mode = SMOOTH_MODE;      break;
+    }
+    IrReceiver.resume();
 }
 
 
 
 void loop() {
-    if (encoderData.isUpdated) execEncoder();
 //    colorMusic();
+//    execEncoder();
+//    execIrCommand(colorModes);
+    execIrCommandTest();
+    switch (colorModes.mode) {
+        case OFF_MODE:
+        case COLOR_MODE: break;
+        case COLOR_MUSIC_MODE: colorMusic(); break;
+//        case FLASH_MODE: rainbowColorMode(colorModes.rainbow); break;
+////        case FLASH_MODE: flashColorMode(colorModes.flash); break;
+//        case STROBE_MODE: strobeColorMode(colorModes.strobe); break;
+//        case FADE_MODE: fadeColorMode(colorModes.fade); break;
+//        case SMOOTH_MODE: smoothColorMode(colorModes.smooth); break;
+    }
 }
 
 
