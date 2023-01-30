06dd561768564f565cf4095a826601026536a2ef
Arthur Koba <caprimores@gmail.com>
2023-01-30 14:31:36 +0300

    default a2dp volume 100%, add callbacks change states connection, audio change, sample rate, metadata

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,32 @@ CRGB leds[NUM_LEDS];
 ColorModes colorModes;
 BluetoothA2DPSink a2dp_sink;
 
+void callbackChangeConnectionState(esp_a2d_connection_state_t state, void *obj) {
+    Serial.println("Change connection state: " + String(state));
+    if (state == ESP_A2D_CONNECTION_STATE_CONNECTED) {
+        Serial.print("Connected host: ");
+        Serial.print(a2dp_sink.get_connected_source_name());
+        Serial.println();
+    }
+
+}
+
+void callbackAVRCMetadata(uint8_t param, const uint8_t *text, int length) {
+    Serial.print("AVRCMetadata: " + String(param) + " ");
+    Serial.write(text, length);
+    Serial.println();
+    for (auto & led : leds) led = CRGB::Black;
+}
+
+void callbackOnChangeAudioState(esp_a2d_audio_state_t state, void *obj) {
+    Serial.println("Change audio state!");
+    FastLED.showColor(CRGB::Black);
+}
+
+void callbackSampleRate(uint16_t sampleRate) {
+    Serial.println("Sample rate: " + String(sampleRate));
+}
+
 void setup() {
     Serial.begin(SERIAL_SPEED);
     setupColorModes(colorModes);
@@ -15,7 +41,12 @@ void setup() {
     a2dp_sink.set_raw_stream_reader(appendSamples);
     a2dp_sink.set_on_volumechange(change_volume);
     a2dp_sink.start(BLUETOOTH_DEVICE_NAME);
-    a2dp_sink.set_volume(64);
+    a2dp_sink.set_volume(127);
+    a2dp_sink.set_on_connection_state_changed(callbackChangeConnectionState);
+    a2dp_sink.set_on_audio_state_changed_post(callbackOnChangeAudioState);
+    a2dp_sink.set_sample_rate_callback(callbackSampleRate);
+    a2dp_sink.set_avrc_metadata_callback(callbackAVRCMetadata);
+//    a2dp_sink.set_task_core(0);
     setupEncoder();
     IrReceiver.begin(IR_RECEIVE_PIN);
     setupColorMusic(fftData);
