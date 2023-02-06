93a5c8a0355f412c58b43c0d9568f102b6f33fcc
Arthur Koba <caprimores@gmail.com>
2023-02-06 15:42:02 +0300

    add FFTColorMusic enabler and disabler, setter, getter configFFT, change sample rate and add samples callback

--- src/colorMusic/colorMusic.cpp
+++ src/colorMusic/colorMusic.cpp
@@ -9,13 +9,38 @@ ColorMusic::ColorMusic() {
 //}
 
 void ColorMusic::enable() {
-    printf("Enable color music. Init FFT\n");
+    printf("ColorMusic enable.\n");
+    this->fft = new FFTColorMusic(fftConfig);
+//    printf("free memory after create: %i\n", ESP.getFreeHeap());
 }
 
 void ColorMusic::disable() {
-    printf("Disable color music. Delete FFT\n");
+    printf("ColorMusic disable.\n");
+    delete this->fft;
+    fft = nullptr;
+//    printf("free memory after delete: %i\n", ESP.getFreeHeap());
 }
 
 void ColorMusic::show() {
+//    vTaskDelay(5000);
+}
 
+void ColorMusic::setSampleRate(uint16_t sampleRate) {
+    printf("sample rate new: %u \n", sampleRate);
+    fftConfig.frequencyStep = 1/((float) SAMPLES_SIZE/ (float) sampleRate);
+    printf("set frequencyStep: %f \n", fftConfig.frequencyStep);
+    if (fft != nullptr) setConfigFFT(fftConfig);
+}
+
+void ColorMusic::addSamples(const uint8_t *data, uint32_t length) {
+    if (fft != nullptr) fft->addSamples(data, length);
+}
+
+FFTConfig ColorMusic::getConfigFFT() {
+    return fftConfig;
+}
+
+void ColorMusic::setConfigFFT(FFTConfig &config) {
+    if (fft != nullptr) fft->setConfigs(config);
+    fftConfig = config;
 }
--- src/colorMusic/colorMusic.h
+++ src/colorMusic/colorMusic.h
@@ -1,9 +1,9 @@
 #ifndef ESP32_COLOR_MUSIC_COLORMUSIC_H
 #define ESP32_COLOR_MUSIC_COLORMUSIC_H
 
-#include "stdio.h"
-//#include "BluetoothA2DPSink.h"
-//#include "fft.h"
+#include <cstdio>
+#include "fft.h"
+#include "BluetoothA2DPSink.h"
 
 class ColorMusic {
 public:
@@ -12,8 +12,16 @@ public:
     void enable();
     void disable();
     void show();
+
+    void setSampleRate(uint16_t sampleRate);
+    void addSamples(const uint8_t *data, uint32_t length);
+
+    FFTConfig getConfigFFT();
+    void setConfigFFT(FFTConfig &config);
 private:
-//    BluetoothA2DPSink *a2dp;
+//    BluetoothA2DPSink *a2dp = nullptr;
+    FFTColorMusic *fft = nullptr;
+    FFTConfig fftConfig;
 };
 
 #endif //ESP32_COLOR_MUSIC_COLORMUSIC_H
