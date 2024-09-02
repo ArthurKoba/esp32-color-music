17e8444f8fce7dc6faa00947908ce9770fcf08ae
Arthur Koba <caprimores@gmail.com>
2024-09-02 14:25:54 +0300

    update color music mode

--- src/visualization/color_music_mode.h
+++ src/visualization/color_music_mode.h
@@ -2,16 +2,24 @@
 #define ESP32_COLOR_MUSIC_COLOR_MUSIC_MODE_H
 
 #if not defined(COLOR_MUSIC_MODE_DEF_DELAY_MS)
-#define COLOR_MUSIC_MODE_DEF_DELAY_MS 15
+#define COLOR_MUSIC_MODE_DEF_DELAY_MS 30
 #endif
 
 #include "color_modes/abs_color_mode.h"
 #include "../audio_analysis/audio_analyzer.h"
 
+struct BrightInfo {
+    uint8_t low;
+    uint8_t middle;
+    uint8_t high;
+};
+
 class ColorMusicMode final: public AbstractColorMode {
+    AudioAnalyzer &_analyzer;
+
     uint16_t _get_show_delay() const override;
     void _calculate(LedController &controller) override;
-    AudioAnalyzer &_analyzer;
+    static BrightInfo _calc_bright_info(AudioInformation &info);
 public:
     explicit ColorMusicMode(AudioAnalyzer &analyzer);
 };
