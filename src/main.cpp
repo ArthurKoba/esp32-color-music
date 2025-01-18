5f522824becd293f8d5cad1b833221b23a5ca6c5
Arthur Koba <caprimores@gmail.com>
2025-01-18 03:33:52 +0300

    add test entry, testing spdif

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#if not defined(TEST_ENTRY)
 #include "app.h"
 
 App app;
@@ -10,5 +11,36 @@ void loop() {
 //    vTaskDelete(nullptr);
     app.tick();
 }
+#else
+#include "configs.h"
+#include "AudioTools.h"
+#include "BluetoothA2DPSink.h"
 
+#define LED_PIN 2
+
+#include "spdif.h"
+
+SPDIFOutput spdif;
+BluetoothA2DPSink a2dp_sink(spdif);
+
+void setup() {
+
+    Serial.begin(SERIAL_SPEED);
+    Serial.println("\nStart Test Entry!");
+
+
+    pinMode(LED_PIN, OUTPUT);
+    SPDIFConfigs configs;
+    configs.pin = gpio_num_t(23);
+    spdif.set_configs(configs);
+
+    a2dp_sink.set_volume(64);
+    a2dp_sink.start("MyMusic");
+}
+
+void loop() {
+    digitalWrite(LED_PIN, not digitalRead(LED_PIN));
+    delay(1000);
+}
+#endif
 
