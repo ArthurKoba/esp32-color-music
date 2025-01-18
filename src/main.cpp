a4bfd24bdb3227145cd9b4177719228d5ada4875
Arthur Koba <caprimores@gmail.com>
2025-01-18 03:34:22 +0300

    remove custom spdif, used from audio tools

--- src/main.cpp
+++ src/main.cpp
@@ -13,34 +13,32 @@ void loop() {
 }
 #else
 #include "configs.h"
-#include "AudioTools.h"
 #include "BluetoothA2DPSink.h"
+#include "AudioTools/AudioLibs/SPDIFOutput.h"
 
-#define LED_PIN 2
 
-#include "spdif.h"
-
-SPDIFOutput spdif;
-BluetoothA2DPSink a2dp_sink(spdif);
+SPDIFOutput stream;
+BluetoothA2DPSink a2dp_sink;
 
 void setup() {
-
     Serial.begin(SERIAL_SPEED);
     Serial.println("\nStart Test Entry!");
 
+    auto cfg = stream.defaultConfig();
+    cfg.buffer_size = 384;
+    cfg.buffer_count = 30;
+    cfg.pin_data = 23;
+    stream.begin(cfg);
 
-    pinMode(LED_PIN, OUTPUT);
-    SPDIFConfigs configs;
-    configs.pin = gpio_num_t(23);
-    spdif.set_configs(configs);
-
+    a2dp_sink.set_output(stream);
+    a2dp_sink.set_on_volumechange(volume_cb);
     a2dp_sink.set_volume(64);
     a2dp_sink.start("MyMusic");
 }
 
 void loop() {
-    digitalWrite(LED_PIN, not digitalRead(LED_PIN));
-    delay(1000);
+    vTaskDelete(nullptr); // Fixed bugs in issue (close arduino loop task without error)
+//    delay(1000); // bugs in issue
 }
 #endif
 
